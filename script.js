ddf95566936fa320f84dd7b6699739854185f8b5
Aevlin Prince <aevlinprincepraa@gmail.com>
2025-10-18 17:20:00 +0530

    initial todo list setup

---
+++ script.js
@@ -0,0 +1,16 @@
+function addTask(){
+  const input =
+    document.getElementById("taskInput");
+
+  const list =
+    document.getElementById("taskList");
+
+  const li =
+    document.createElement("li");
+
+  li.innerText = input.value;
+
+  list.appendChild(li);
+
+  input.value = "";
+}
